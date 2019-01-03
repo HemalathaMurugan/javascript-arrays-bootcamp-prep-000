82bf1f2fc1637019bd0e3eca736971ffa02d763b
Hemalatha M <hemalatha.psna@gmail.com>
2019-01-03 05:50:57 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -3,14 +3,21 @@ function addElementToBeginningOfArray(array,element){
   return [element,...array];
   
 }
+<<<<<<< HEAD
 function destructivelyAddElementToBeginningOfArray(array, element) {
   array.unshift(element);
    return array;
+=======
+function destructivelyAddElementToBeginningOfArray(array,element){
+  return [element,...array];
+  
+>>>>>>> 8840658a3f4f059a9b99e5dc232364ae89941546
 }
 function addElementToEndOfArray(array,element){
   return [...array,element];
   
 }
+<<<<<<< HEAD
 function destructivelyAddElementToEndOfArray(array, element) {
    array.push(element);
    return array;
@@ -30,6 +37,28 @@ function removeElementFromBeginningOfArray(array){
 function destructivelyRemoveElementFromEndOfArray(array) {
    array.pop();
    return array;
+=======
+function destructivelyAddElementToEndOfArray(array,elemnt){
+  array.unshift();
+  return array;
+}
+function accessElementInArray(array,index){
+  //array=[1,2,3,4,5]
+  return array[index];
+}
+function destructivelyRemoveElementFromBeginningOfArray(array){
+array.push();
+return array;
+}
+function removeElementFromBeginningOfArray(array){
+return array.slice[1];
+//  return array1;
+  //return array;
+}
+function destructivelyRemoveElementFromEndOfArray(array) {
+  array.pop();
+  return array;
+>>>>>>> 8840658a3f4f059a9b99e5dc232364ae89941546
     }
 function removeElementFromEndOfArray(array) {
   var array1=array.slice(0,array.length-1);
